791002bf9d429458a93ce3ab458222079f2a7d1c
Alejandro6111 <alejandro.soche10@gmail.com>
2025-05-02 21:43:11 -0500

    Se empieza a construir Clase cliente y constructor

---
+++ src/model/Cliente.java
@@ -0,0 +1,16 @@
+package model; // Indica que esta clase pertenece al paquete model
+
+import java.util.ArrayList; // Necesitamos importar la clase ArrayList para la lista de registradores
+import java.util.List; // Importar List tambien
+
+public class Cliente {
+
+    // Atributos del cliente 
+    // Numero unico, tipo id, correo, direccion
+    private long numeroIdentificacion; // Usamos long para numeros de identidicacion potencialmente grandes
+    private String tipoIdentificacion;
+    private String correoElectronico;
+    private String direccionFisica;
+    
+    
+}
