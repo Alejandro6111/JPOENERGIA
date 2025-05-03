27431f9b16a00d17cb7ed851ee3cfd49f24f1426
Alejandro6111 <alejandro.soche10@gmail.com>
2025-05-02 22:36:56 -0500

    Se acaba el trabajo en modelo, se empieza a trabajar controlador

---
+++ src/model/Consumo.java
@@ -0,0 +1,22 @@
+package model; // Pertece al paquete model
+
+import java.time.LocalDateTime; // Para manejar fecha y hora
+
+
+public class Consumo {
+    // Atributos del consumo 
+    private LocalDateTime fechaHora; // Momento exacto en que se registra el consumo
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
